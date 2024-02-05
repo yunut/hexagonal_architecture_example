2bdd1ad30dbb58eac3593c7101e566096d99ffc4
yunut1 <yunut@catchfashion.com>
2024-02-05 20:48:31 +0900

    project init

---
+++ README.md
@@ -0,0 +1,8 @@
+# hexagonal_architecture_example
+ - hexagonal architecture 예제 프로젝트
+   - TODO 
+     - docker-compose 환경 구축
+     - hexagonal architecture 기반의 rest api 구현
+     - jpa 연동
+     - kotest, docker-container를 이용한 단위테스트, 통합테스트 구현
+     - gradle multi project 구성
---
+++ src/main/kotlin/com/example/hexagonal/HexagonalArchitectureExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.hexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HexagonalArchitectureExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<HexagonalArchitectureExampleApplication>(*args)
+}
